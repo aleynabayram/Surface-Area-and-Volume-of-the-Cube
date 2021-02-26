4d97c1d32b9454e154915fb88d336974d28172ec
Aleyna <aleynabayram.eee@gmail.com>
2021-02-26 13:50:48 +0300

    File created

---
+++ surface area and volume of the cube.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+	int main () {
+
+	float length;
+	float area;
+	float volume;
+
+	printf("Please enter the length of cube:");
+	scanf("%f" , &length);
+
+	area= 6*length*length;
+	volume= length*length*length;
+	
+	printf("The surface area of cube is %.2f\n" ,area);
+	printf("The volume of cube is %.2f" ,volume);
+	
+	return 0;
+}
